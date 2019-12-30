510723675cbb14ea88d62b87d35754198d3c7716
michaeldriscoll <michaelndriscoll81@gmail.com>
2019-12-30 18:20:03 -0500

    getting API data, setting up models next

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Route/*, Link*/ } from "react-router-dom";
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <h3 className="header"></h3>
+          <main>
+          <Route path="" component= />
+          </main>
+          <div className="footer">
+            <p></p>
+            <h3></h3>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
